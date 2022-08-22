34d05e5e1c7cb0ff2caf6b750262a2ef2f442c04
Semih Güzel <71209213+semihguezel@users.noreply.github.com>
2022-08-22 10:44:01 +0300

    Update homework_3.sql

--- SQL/homework_3.sql
+++ SQL/homework_3.sql
@@ -5,7 +5,7 @@ SELECT * FROM country
 WHERE country LIKE '%n' AND length(country) >= 6;
 
 SELECT title FROM film
-WHERE title ILIKE 'T%T%T%T%';
+WHERE title ILIKE '%T%T%T%T%';
 
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
